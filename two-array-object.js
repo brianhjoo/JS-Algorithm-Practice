48ee7763f122c3d053eb0af94db72b341ec583ac
Brian Joo <brianhjoo@gmail.com>
2023-02-24 16:32:10 -0800

    finished twoArrayObject algorithm

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,23 @@
-// add whatever parameters you deem necessary & write docstring
-function twoArrayObject() {
+'use strict';
+
+/** Takes in two arrays of keys and values, returns and object with paired
+ * keys and values.
+ */
+
+function twoArrayObject(keys, values) {
+  const newObj = {};
+  let index = 0;
+
+  for (let key of keys) {               // O(n)
+    newObj[key] = null;
+  }
+
+  for (let key in newObj) {             // O(n)
+    if (index <= values.length - 1) {
+      newObj[key] = values[index];
+      index++;
+    }
+  }
+
+  return newObj;
 }
