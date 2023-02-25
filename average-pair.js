309171d3dcfca049f96d2f841748fcc0f4dc7d5e
Justin Clark <jclarksummit@gmail.com>
2023-02-24 19:05:30 -0500

    Finished first two algo problems

--- average-pair.js
+++ average-pair.js
@@ -1,4 +1,28 @@
-// add whatever parameters you deem necessary & write docstring
-function averagePair() {
+'use strict';
+
+/* Takes an array and target average. If any two values in array are equal to the target,
+ * returns true, otherwise return false
+ **/
+function averagePair(nums, targetAvg) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    let currAvg = (nums[left] + nums[right]) / 2;
+
+    if (currAvg === targetAvg) {
+      return true;
+    }
+
+    if (currAvg < targetAvg) {
+      left++;
+    } else {
+      right--;
+    }
+
+  }
+
+  return false;
+
 }
 
