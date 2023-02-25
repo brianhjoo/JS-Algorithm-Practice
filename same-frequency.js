1753b378f502a655667c80827bf24e1df1597e38
Justin Clark <jclarksummit@gmail.com>
2023-02-24 19:51:58 -0500

    Finished same-frequency.js problem

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,45 @@
-// add whatever parameters you deem necessary & write docstring
-function sameFrequency() {
+'use strict';
+
+/** Takes in two numbers and returns true if both numbers have the same
+ * frequency of digits
+ */
+function sameFrequency(num1, num2) {
+
+  if (num1.length !== num2.length) {
+    return false;
+  }
+
+
+  let string1 = num1.toString()
+  let string2 = num2.toString()
+
+  const freq1 = generateFrequencyCounter(string1);
+  const freq2 = generateFrequencyCounter(string2);
+
+  for (let num in freq1) {
+
+    if (!num in freq2) {
+      return false;
+    }
+
+    if (freq1[num] !== freq2[num]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+
+/** Takes in an iterable and generates a frequency counter */
+
+function generateFrequencyCounter(items) {
+  const freqTracker = {};
+
+  for (let item of items) {
+    const counter = freqTracker[item] || 0;
+    freqTracker[item] = counter + 1;
+  }
+
+  return freqTracker;
 }
